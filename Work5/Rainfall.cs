9e62208a7ac24218c6c7b7d2fa9f2fadc53b44b2
shiqing.feng <shiqing.feng@starlight-sms.com>
2020-08-18 15:11:56 +0800

    add work5

---
+++ Work5/Rainfall.cs
@@ -0,0 +1,109 @@
+using System;
+using System.IO;
+
+namespace WeatherAnalysis
+{
+    public class Rainfall
+    {
+        /// <summary>
+        ///     Test driver for PrintRainfallSummary.
+        ///     Creates a TextReader attached to a text stream which can be 
+        ///     used to read tabulated rainfall data for a single year.
+        /// </summary>
+        /// <param name="args">
+        ///     A string array containing command line arguments. If an argument 
+        ///     is supplied, it is a valid path to a file containing rainfall 
+        ///     data. Otherwise, the program will use hard-coded data defined
+        ///     in-line in the code.
+        /// </param>
+        public static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                string path = args[0];
+                using (TextReader reader = new StreamReader(args[0]))
+                {
+                    PrintRainfallSummary(path, reader);
+                }
+            }
+            else
+            {
+                string path = "C:\\Path\\To\\File\\three_days_data.csv";
+                string threeDaysData = @"Product code,Bureau of Meteorology station number,Year,Month,Day,Rainfall amount (millimetres),Period over which rainfall was measured (days),Quality
+IDCJAC0009,040913,2012,01,01,2.4,1,N
+IDCJAC0009,040913,2012,01,02,,,
+IDCJAC0009,040913,2012,01,03,0.0,1,N";
+
+                using (TextReader reader = new StringReader(threeDaysData))
+                {
+                    PrintRainfallSummary(path, reader);
+                }
+            }
+        }
+
+        /// <summary>
+        ///     Parse the rainfall data and compute a summary according to 
+        ///     specification. Results are written to standard output stream.
+        /// <para>
+        ///     Refer to question text and sample data files for specifics.
+        /// </para>
+        /// </summary>
+        /// <param name="filePath">
+        ///     A string containing the path to the data file. This is for
+        ///     display purposes only. Do not attempt to open this file.
+        /// </param>
+        /// <param name="reader">
+        ///     A TextReader which has been attached to the data file and is 
+        ///     ready to parse content.
+        /// </param>
+        /// <returns>
+        ///		This method does not return a value.
+        /// </returns>
+        //)
+
+        // INSERT METHOD HERE. 
+        public static void PrintRainfallSummary(string filePath, TextReader reader)
+        {
+            Console.WriteLine($"Reading data from {filePath}...");
+            Console.WriteLine();
+            string line;
+            int count = 0, record = 0;
+
+            string year = "2020";
+            decimal total = 0, average=0 ,max=0 , miss = 0;
+            while ((line = reader.ReadLine()) != null)
+            {
+                count++;
+                if (count < 2)
+                {
+                    continue;
+                }
+                var arrs = line.Split(",");
+                year = arrs[2];
+
+                string rainStr = arrs[5];
+                decimal rainAmount = 0;
+                var flag = decimal.TryParse(rainStr, out rainAmount);
+                if (flag)
+                {
+                    max = max > rainAmount?max:rainAmount;
+                    total += rainAmount;
+                    record++;
+                }
+                else
+                {
+                    miss++;
+                }
+            }
+
+            average = Math.Round(total / record, 2);
+            miss = Math.Round(miss / (count-1), 2);
+            Console.WriteLine($"Year: {year}");
+            Console.WriteLine($"Totla Rainfall: {total.ToString("#0.00")} mm");
+            Console.WriteLine($"Average Rainfall: {average.ToString("#0.00")} mm / day");
+            Console.WriteLine($"Maximum Daily Rainfall: {max.ToString("#0.00")} mm");
+            Console.WriteLine($"Missing Data: {miss.ToString("#0.00")}%");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Work5/Sequence.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Sequence
+{
+    static void main(string[] args)
+    {
+        var result = Kmers(3, "AGATCGATGTG");
+        foreach (var item in result)
+        {
+            Console.WriteLine(item);
+        }
+        string seq = "ACTGA";
+        ReverseComplement(ref seq);
+        Console.WriteLine(seq);
+    }
+
+    public static string[] Kmers(int k, string seq)
+    {
+        if (k > seq.Length) return null;
+
+        int count = seq.Length - k + 1;
+        string[] result = new string[count];
+
+        for (int i = 0; i < count; i++)
+        {
+            result[i] = seq.Substring(i, k);
+        }
+        return result;
+    }
+
+    public static void ReverseComplement(ref string seq)
+    {
+        Dictionary<char, char> format = new Dictionary<char, char>
+        {
+            {'A','T' },
+            {'T','A' },
+            {'C','G' },
+            {'G','C' }
+        };
+
+        char[] result = new char[seq.Length];
+        var seqArrs = seq.ToCharArray();
+        for (int i = seqArrs.Length - 1; i >= 0 ; i--)
+        {
+            if (format.ContainsKey(seqArrs[i]))
+            {
+                result[seqArrs.Length - 1 - i] = format[seqArrs[i]];
+            }
+        }
+        seq = new string(result);
+    }
+
+    public static bool Dyad(string seq, int num = 5)
+    {
+        var seqArrs = seq.Split("...");
+        if(seqArrs.Length == 2 && !seqArrs.Contains(".") && seqArrs[1].Length <= 5 && seqArrs[0].Length <= 5)
+        {
+            string tmp = seqArrs[0];
+            ReverseComplement(ref tmp);
+            if (tmp.Equals(seqArrs[1])) return true;
+        }
+        return false;
+    }
+}
